0d4c55d9b871cf93e9dec4aea06107b094683ab5
ryan <ryandehallo229@gmail.com>
2022-06-08 16:57:58 -0300

    Event: update, remove, list;

--- services/controllers/eventController/event.js
+++ services/controllers/eventController/event.js
@@ -26,8 +26,90 @@ const register = async (name, datetime, description, peopleNumber) => {
     }
 }
 
+const getAll = async () => {
 
+    return await EventList.findAll()
+}
+
+
+const remove = async (name) => {
+
+    const event = await EventList.findOne({where: {name_event: name}})
+
+    if(!event){
+        let message = "Event not found!"
+        return message
+    }
+    
+    await EventList.destroy({where: {id_event: event.id_event}})
+
+    let message = "Event has been remove"
+    return message
+}
+
+
+const update = async (name, datetime, description, peopleNumber) => {
+    
+    const requestData = []
+
+    const date = new Date(datetime)
+    
+    const time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+    const date_event = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay()
+
+
+    const event = await EventList.findOne({where: {name_event: name}})
+
+    requestData.push(name, date_event, time, description, peopleNumber)
+
+
+    for(let index in requestData){
+        if (requestData[index] == undefined) {
+            continue
+
+        }else{
+            switch(index){
+
+                case '0':
+                    event.name_event = requestData[index]
+                    event.save()
+                    break
+
+                case '1':
+                    event.date_event = requestData[index]
+                    event.save()
+                    break
+
+                case '2':
+                    event.time_event = requestData[index]
+                    event.save()
+                    break
+
+                case '3':
+                    event.event_description = requestData[index]
+                    event.save()
+                    break
+
+                case '4':
+                    event.people = requestData[index]
+                    event.save()
+                    break
+
+                default:
+                    break
+            }
+        }
+
+    }
+    
+    
+    
+    
+}
 
 module.exports = {
-    register
+    register,
+    getAll,
+    remove,
+    update,
 }
--- services/controllers/eventController/eventController.js
+++ services/controllers/eventController/eventController.js
@@ -11,10 +11,35 @@ router.post('/register', async (req, res) => {
     const { name, datetime, description, peopleNumber } = req.body
     console.log(datetime)
     
-    event.register(name ,datetime, description, peopleNumber)
+    await event.register(name ,datetime, description, peopleNumber)
 
     res.status(200).end()
 
+})
+
+router.get('/list', async (req, res) => {
+    
+    const events = await event.getAll()
+
+    res.json(events)
+})
+
+
+router.delete('/remove', async (req, res) => {
+    const { name } = req.body
+
+    let message = await event.remove(name)
+    
+    res.json({"message": message})
+
+})
+
+router.put('/update', async (req, res) => {
+    const { name, datetime, description, peopleNumber } = req.body
+
+    await event.update(name, datetime, description, peopleNumber)
+
+    res.status(200).end()
 })
 
 module.exports = router
